1b467424eb243933562f1cc6f171ce0bc0d626cc
JFMajer <jakubmajer@live.com>
2023-05-23 18:46:22 +0200

    more subnets

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {}
 
 locals {
   app_name = "polar"
-  vpc_cidr = "10.0.23.0/16"
+  vpc_cidr = "10.23.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 }
 
